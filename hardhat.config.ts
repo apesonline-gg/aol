055b05272768a823efff3abae1a271617a25dcfb
valmack <8445610+valmack@users.noreply.github.com>
2021-05-12 23:04:35 -0400

    feat: add keys and infura to dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,25 +1,37 @@
-import { HardhatUserConfig, task } from 'hardhat/config'
-import '@nomiclabs/hardhat-waffle'
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 
-import '@openzeppelin/hardhat-upgrades'
-import "hardhat-gas-reporter"
+import "@openzeppelin/hardhat-upgrades";
+import "hardhat-gas-reporter";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (args, hre) => {
-  const accounts = await hre.ethers.getSigners()
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address)
+    console.log(account.address);
   }
-})
+});
+
+const AOLDeployerSK = process.env.AOL_DEPLOYER_PRIVATE_KEY || "";
+const infuraRinkebyUrl = process.env.INFURA_RINKEBY_URL;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.2",
   gasReporter: {
     currency: "USD",
     enabled: process.env.REPORT_GAS == "true"
+  },
+  networks: {
+    rinkeby: {
+      url: infuraRinkebyUrl,
+      accounts: [AOLDeployerSK]
+    }
   }
 }
 
-export default config
+export default config;
