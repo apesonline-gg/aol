70d217286b083969b83c5873545ebf8c4d6905db
valmack <8445610+valmack@users.noreply.github.com>
2021-05-16 18:48:10 -0400

    feat: add Modem v0 contract

---
+++ contracts/MODEMv0.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/presets/ERC20PresetFixedSupplyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract MODEMv0 is ERC20PresetFixedSupplyUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 public constant version = 0;
+
+    function initialize(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address owner
+    ) public virtual override initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        __ERC20PresetFixedSupply_init(name, symbol, initialSupply, owner);
+        transferOwnership(owner);
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+}
+
